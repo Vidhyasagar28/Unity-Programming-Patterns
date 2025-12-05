b005be53da4d506632a757598264b0e0247f5bd8
Vidhya Sagar <95131700+Vidhyasagar28@users.noreply.github.com>
2025-12-06 04:21:08 +0530

    Delete BaseState.cs

--- BaseState.cs
+++
@@ -1,7 +0,0 @@
-public  abstract class BaseState 
-{
-   public abstract void EnterState(StateManager state);
-   public abstract void UpdateState(StateManager state);
-   public abstract void OnCollisionEnter(StateManager state);
-
-}
